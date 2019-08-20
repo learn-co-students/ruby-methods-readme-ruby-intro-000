6f0a47c032e90861b2e624afe60215fe2864368e
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-20 01:00:33 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,32 @@
 # Use this space to code along with the readme
+phrase = "Hello World"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
+
+def greeting
+  puts "Hello World"
+end
