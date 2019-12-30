f4c9a3788399d30e87f9431d7280ac6ad72528c5
Franchell Polanco <polanco.franchell@gmail.com>
2019-12-30 06:46:26 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-
+def say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +11,8 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+say_hello_world_ten_times
+def greeting
+  puts "Hello World"
+end
