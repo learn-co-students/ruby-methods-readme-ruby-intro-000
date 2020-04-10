a20657129eddbc3b526b48f3061d43c88d33424b
Avni Alp Mumcu <a.alp.mumcu@gmail.com>
2020-04-10 21:25:00 +0000

    Done.

---
+++ lib/test.rb
@@ -0,0 +1,5 @@
+def aydin_dean_birthday
+ puts "09-20-2020 is Aydin Dean's Birthday"
+end
+
+aydin_dean_birthday
---
+++ multiple.rb
@@ -0,0 +1,17 @@
+#This is a method exercise
+
+def multiple_aydin_dean_method_exercise
+  puts "I love Aydin Dean So Much"
+  puts "I love Aydin Dean So Much"
+  puts "I love Aydin Dean So Much"
+  puts "I love Aydin Dean So Much"
+  puts "I love Aydin Dean So Much"
+  puts "He is apple of my eyes"
+  puts "He is apple of my eyes"
+  puts "He is apple of my eyes"
+
+end
+
+multiple_aydin_dean_method_exercise
+multiple_aydin_dean_method_exercise
+multiple_aydin_dean_method_exercise
