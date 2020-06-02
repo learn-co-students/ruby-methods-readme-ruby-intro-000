56509a0af1b12599d1b8afe9da228e4d474933f0
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-06-02 09:11:27 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,17 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
