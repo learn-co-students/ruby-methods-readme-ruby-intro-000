9eb0a4d4628f26e2ef30e4b6f58a0d582032b82b
munificent-forum-1695 <bunchshawn@gmail.com>
2020-07-23 17:59:46 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,26 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+puts say_hello_world_ten_times
+puts say_hello_world_ten_times
+puts say_hello_world_ten_times
+puts say_hello_world_ten_times
+
+def greeting
+  puts "Hello World!"
+end
+
+puts greeting
