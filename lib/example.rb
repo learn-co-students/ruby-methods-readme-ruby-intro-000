d3c736ff42f48523bc929a0e12c1438ff2859012
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-04 21:51:48 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,16 @@
 # Use this space to code along with the readme
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+say_hello_world_ten_times
