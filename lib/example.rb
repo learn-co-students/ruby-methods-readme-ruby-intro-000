d8a5cfddf0479dde214921bc07b7e218a9497d51
Dominic DeJesus <domludejesus@gmail.com>
2020-09-03 15:12:59 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,7 @@
-# Use this space to code along with the readme
+def greeting 
+  puts "Hello World"
+end 
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+greeting 
+greeting 
+greeting 
