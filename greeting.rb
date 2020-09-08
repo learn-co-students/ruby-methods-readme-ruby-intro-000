2cadb63c2c763c0a52dc166c8c821db6896db9a5
DavidRMorphew <morphewd@umich.edu>
2020-09-08 15:46:33 -0400

    Done.

--- greeting.rb
+++ greeting.rb
@@ -7,3 +7,16 @@ greeting
 greeting
 greeting
 greeting
+
+def greeting_loop(num_of_greetings)
+  i = 0
+  until i == num_of_greetings
+    puts "Hello World!"
+    i += 1
+  end
+end
+
+puts "How many greetings would you like?"
+
+num_of_greetings = gets.strip.to_i
+greeting_loop(num_of_greetings)
