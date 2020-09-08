cb6039f1cf1a9e50da26a1354680899e7c57c1a5
DavidRMorphew <morphewd@umich.edu>
2020-09-08 15:29:24 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -13,3 +13,13 @@ phrase = "Hello World!"
   puts phrase
   puts phrase
 end
+
+def say_hello_ten_times_loop
+  phrase = "Hello World!"
+  i = 0
+  until i == 10
+    puts phrase
+    i += 1
+  end
+end
+say_hello_ten_times_loop
