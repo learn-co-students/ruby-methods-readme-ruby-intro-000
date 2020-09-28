c232996d3b3932b02da39f9e7134b40e43397d6e
Ify Nwakudu <amandanwakudu@gmail.com>
2020-09-28 20:40:17 +0000

    Done.

---
+++ greeting.rb
@@ -0,0 +1,19 @@
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
+
+def name
+  puts "Ifeoma"
+  puts "Amanda"
+  puts "Nwakudu"
+end
+
+name 
+name 
+name
---
+++ intro.rb
@@ -0,0 +1,7 @@
+def greeting
+  users = ["Hannah","Morgan","Jody"]
+  puts "Hello" "users [1]", "Welcome Back!"
+end
+
+greeting
+  
