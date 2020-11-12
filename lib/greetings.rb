ab7c993017a122806e120f088a1e026c5f76aa7c
darren a vinson jr <darrenvinson@gmail.com>
2020-11-12 16:48:08 +0000

    Done.

---
+++ lib/greetings.rb
@@ -0,0 +1,13 @@
+def says_hello_ten_times
+ phrase = 'whats Popping?'
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+end
