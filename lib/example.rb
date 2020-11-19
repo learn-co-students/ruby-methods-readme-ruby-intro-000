d2228ec759761c479007685459ab42f18024bf50
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-11-19 22:06:00 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,18 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+def greeting
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+greeting
