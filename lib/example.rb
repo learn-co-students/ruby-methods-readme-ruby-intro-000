b8a6118bcbb32f257b4c90d75cc035eb99b2e2af
Kalin Jack <kalinj1997@gmail.com>
2020-12-08 01:05:43 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,5 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+  puts phrase
+end
