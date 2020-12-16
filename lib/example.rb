0d72df6015a718530ad0bc80ebd5219faec64584
honorable-architecture-d4bb <damellfaines8@gmail.com>
2020-12-16 11:33:50 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,8 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+
 puts phrase
 puts phrase
 puts phrase
@@ -11,3 +13,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
