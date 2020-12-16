10eba51ded8f9f7ee704684facef6e0e0c5c5f49
Sam Pattison <sdpstrength@gmail.com>
2020-12-16 00:52:21 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,19 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
