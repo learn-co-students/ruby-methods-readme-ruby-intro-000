e016e9f083f358630047695681d461a4ceed1915
James Jensen <jnjensen@live.com>
2021-01-20 03:50:50 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,6 @@
 # Use this space to code along with the readme
+def greeting
+  puts "Hello World!"
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+greeting
