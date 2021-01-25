58486ce60fc3e17a9c7aaf943eba88e6caaa57be
Anna <ajlukasik@gmail.com>
2021-01-25 14:52:26 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,19 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_helle_world_ten_times
+  phrase = "Hello World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_helle_world_ten_times
