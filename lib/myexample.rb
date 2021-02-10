328024e364d4320d1c84c26f59984dc0187352a5
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-10 21:44:26 +0000

    Done.

---
+++ lib/myexample.rb
@@ -0,0 +1,13 @@
+def say_hello_ten_times
+  phrase = "Hello World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
