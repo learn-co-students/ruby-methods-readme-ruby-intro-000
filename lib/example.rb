286d0ec07cb07c8fea49373002857b2add614477
Ryan Hardin <rhardin378@gmail.com>
2021-02-24 03:55:38 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,19 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times #defining phrase we want to manipulate
+  phrase = "Hello World!" #phrase we want to display
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+
+puts say_hello_world_ten_times
