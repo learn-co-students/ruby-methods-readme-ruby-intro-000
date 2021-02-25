e21d2221347e30429ed58b1bd93df5a010af499c
Leandran M <websitesa95@gmail.com>
2021-02-25 22:24:51 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,28 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+def greeting # Method Signature
+  puts "Hello World" # Method Body
+end # Method Closing
+
+
+say_hello_world_ten_times
+
+greeting
+greeting
+greeting
+greeting
+greeting
