c29f4715dc12cc2bc7d09284c18243f15a40dfab
smiling-domain-3d0d <mann.of.1018@gmail.com>
2021-03-02 00:37:57 +0000

    Done.

---
+++ lib/greeting.rb
@@ -0,0 +1,14 @@
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+say_hello_world_ten_times
