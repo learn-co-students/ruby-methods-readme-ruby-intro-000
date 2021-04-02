db55618343729327e66e7663cd2cc5f8ddcbe601
SKY FLAHERTY <flahertysky12@gmail.com>
2021-04-02 22:31:02 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,26 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_i_love_coding
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+  puts "Flatiron"
+end
+
+
+def greeting
+  puts "Hello World"
+end
+
+
+
+
+  
